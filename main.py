a48b8825f7bbde7d02fdc461fc779c4c1ed267ea
duduzd <karleandersonzd@gmail.com>
2026-07-30 17:14:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,4 +81,7 @@ async def analise():
 
 @app.get("/health")
 def health():
-    return {"status": "online", "service": "srv-d9kehcegekts73ctdavg"}
+    return {"status": "online", "service": "srv-d9kehcegekts73ctdavg"}if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
